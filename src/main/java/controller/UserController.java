846543e7b0ca8e50cdf4b091eb1fa4edb40fd3f8
Kim Jun Ha <junhaha3@gamil.com>
2024-06-01 00:23:26 +0900

    유저 액션 동작들 추가하여 코드 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -14,7 +14,8 @@ import javax.servlet.http.*;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	static final int LISTCOUNT = 5; 
+	static final int LISTCOUNT = 8; 
+	static final int NOTICECOUNT = 3; 
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
@@ -29,12 +30,31 @@ public class UserController extends HttpServlet {
 		response.setContentType("text/html; charset=utf-8");
 		request.setCharacterEncoding("utf-8");
 	
-		if (command.equals("/LoginAction.userdo")) {
+		if (command.equals("/BoardWriteAction.userdo")) { //일반 사용자 게시글 작성
+			requestBoardWrite(request);
+			RequestDispatcher rd;
+			rd = request.getRequestDispatcher("/AllBoardListAction.userdo");
+			rd.forward(request, response);
+		} else if (command.equals("/BoardListAction.userdo")) {//일반 사용자 게시글만 반영하여 메인 페이지로
 			requestBoardList(request);
 			RequestDispatcher rd;
-			rd = request.getRequestDispatcher("./adminPage/boardmanage_admin.jsp");
+			rd = request.getRequestDispatcher("/userPage/mainpage.jsp");
 			rd.forward(request, response);
-		} 
+		} else if (command.equals("/NoticeBoardListAciton.userdo")) { //공지용 게시판만 반영하여 메인 페이지로
+			requestNoticeList(request);
+			
+			RequestDispatcher rd;
+			rd = request.getRequestDispatcher("/userPage/mainpage.jsp");
+			rd.forward(request, response); 
+			
+		} else if(command.equals("/AllBoardListAction.userdo")) { //모든 게시판 보여주기
+			requestBoardList(request);
+			requestNoticeList(request);
+			
+			RequestDispatcher rd;
+			rd = request.getRequestDispatcher("/userPage/mainpage.jsp");
+			rd.forward(request, response); 
+		}
 	}
 	
    //새로운 글 등록하기
@@ -56,41 +76,6 @@ public class UserController extends HttpServlet {
 		board.setSecondadd(request.getParameter("add2"));
 			
 		dao.insertBoard(board);								
-	}
-	//공지 게시글 보여주기
-	public void requestNoticeList(HttpServletRequest request) {
-		BoardDao dao = BoardDao.getInstance();
-		ArrayList<Board> boardlist = new ArrayList<Board>();
-		
-		int pageNum=1;
-		int limit=LISTCOUNT;
-		String name = "'admin'";
-		
-		if(request.getParameter("pageNum")!=null)
-			pageNum=Integer.parseInt(request.getParameter("pageNum"));
-		
-		int total_record = dao.getListCount(name); // 총 게시글 갯수
-		boardlist = dao.getBoardList(pageNum, limit, name); 
-		
-		int total_page; // 갯수
-		
-		if (total_record % limit == 0){     
-	     	total_page =total_record/limit;
-	     	Math.floor(total_page);  
-		}
-		else{
-		   total_page =total_record/limit;
-		   Math.floor(total_page); 
-		   total_page =  total_page + 1; 
-		}		
-   
-   		request.setAttribute("pageNum", pageNum);		  
-   		request.setAttribute("total_page", total_page);   
-		request.setAttribute("total_record",total_record); 
-		request.setAttribute("boardlist", boardlist);		
-		request.setAttribute("type", "notice");
-		
-		System.out.println("check");
 	}
 	//일반 게시글 보여주기
 	public void requestBoardList(HttpServletRequest request){
@@ -122,6 +107,37 @@ public class UserController extends HttpServlet {
    		request.setAttribute("total_page", total_page);   
 		request.setAttribute("total_record",total_record); 
 		request.setAttribute("boardlist", boardlist);
-		request.setAttribute("type", "common");
+	}
+	
+	public void requestNoticeList(HttpServletRequest request) {
+		BoardDao dao = BoardDao.getInstance();
+		ArrayList<Board> boardlist = new ArrayList<Board>();
+		
+		int pageNum=1;
+		int limit=NOTICECOUNT;
+		String name = "'admin'";
+		
+		if(request.getParameter("pageNum_notice")!=null)
+			pageNum=Integer.parseInt(request.getParameter("pageNum_notice"));
+		
+		int total_record = dao.getListCount(name); // 총 게시글 갯수
+		boardlist = dao.getBoardList(pageNum, limit, name); 
+		
+		int total_page; // 갯수
+		
+		if (total_record % limit == 0){     
+	     	total_page =total_record/limit;
+	     	Math.floor(total_page);  
+		}
+		else{
+		   total_page =total_record/limit;
+		   Math.floor(total_page); 
+		   total_page =  total_page + 1; 
+		}		
+   
+   		request.setAttribute("pageNum_notice", pageNum);		  
+   		request.setAttribute("total_page_notice", total_page);   
+		request.setAttribute("total_record_notice",total_record); 
+		request.setAttribute("noiceboardList", boardlist);		
 	}
 }
