3b383c25d06530ff5c2624220dab71473da5c4bd
Kim Jun Ha <junhaha3@gamil.com>
2024-06-05 05:17:55 +0900

    댓글 데이터를 담기위한 클래스

---
+++ src/main/java/model/Coment.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Coment {
+	private int CNUM;
+	private int BID;
+	private String id;
+	private String content;
+	private LocalDateTime regdate;
+	private String pid;
+	
+	
+	public int getCNUM() {
+		return CNUM;
+	}
+	public void setCNUM(int cNUM) {
+		CNUM = cNUM;
+	}
+	public int getBID() {
+		return BID;
+	}
+	public void setBID(int bID) {
+		BID = bID;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public LocalDateTime getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(LocalDateTime regdate) {
+		this.regdate = regdate;
+	}
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+}
